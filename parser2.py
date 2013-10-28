3ffa903121a9dfaa0e8cbdcb8db4b47f8f9ab633
ryan1234 <ryan1234@gmail.com>
2013-10-28 04:15:27 +0000

    Finished the parsing of rides

--- parser2.py
+++ parser2.py
@@ -11,8 +11,55 @@ c.perform()
 
 soup = BeautifulSoup(html.getvalue())
 
-#for table in soup.find_all('table'):
-	#print(table)
+for row_index, row in enumerate(soup.find_all('table')[6].find_all('tr')):
+	if row_index > 0:
+		new_row = []
+	
+		for cell_index, cell in enumerate(row.find_all('td')):
+			if (cell_index == 0):
+				new_row.append(cell.get_text())
 
-state = soup.find_all('table')[6].find_all('tr')[1].find_all('td')[0]
-print state.get_text()
+			if (cell_index == 1):
+				new_row.append(cell.get_text()[:10])
+
+			if (cell_index == 3):
+				new_row.append(cell.find_all('a')[0].get_text())
+				
+				miles = cell.find_all('font')[0].find_all('font')[0].get_text()
+				new_row.append(miles.replace(u" miles\xa0\xa0\xa0\xa0\xa0\xa0", ""))				
+
+				elevation = cell.find_all('font')[0].get_text().replace(miles, "")
+				new_row.append(elevation.replace(u" ft\xa0\xa0\xa0\xa0\xa0\xa0", ""))
+
+			if (cell_index == 4):
+				time = cell.get_text()
+				if (len(time) > 0):
+					new_row.append(time.split(' ')[0])
+					new_row.append(time.split(' ')[1])
+				else:
+					new_row.append("0:00:00")
+					new_row.append("0:00:00")
+
+			if (cell_index == 7):
+				calories = cell.get_text()
+
+				if (len(calories) > 0):
+					new_row.append(cell.get_text())
+				else:
+					new_row.append("0")
+
+			if (cell_index == 8):
+				hrm = cell.get_text()
+				
+				if (len(hrm) > 0):
+					if ("/" in hrm):
+						new_row.append(hrm.split('/')[0])
+						new_row.append(hrm.split('/')[1])
+					else:
+						new_row.append(hrm)
+						new_row.append("0")
+				else:
+					new_row.append('0')
+					new_row.append('0')
+
+		print new_row
